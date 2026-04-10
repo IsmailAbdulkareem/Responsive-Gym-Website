6e3d633e27538f910d62aa16321de952b0f3ef6b
Ismail <ismail233290@gmail.com>
2026-04-10 13:17:18 +0500

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -39,7 +39,7 @@ const Contact: React.FC = () => {
               frameBorder="0"
               style={{ border: 0, borderRadius: '8px' }}
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3617.7766661215387!2d74.29168!3d24.81755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f0!1s0x3f0!5e0!3m2!1sen!2s!4v123456"
-              allowFullScreen=""
+              allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
               title="GYM Fitness Hub Location - Korangi, Karachi"
